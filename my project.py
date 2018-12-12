32c44d5bc49de19ade9654fc38b01d63fad9db96
[mike] <[mikemiller0328@gmail.com]>
2018-12-12 15:44:07 +0000

    Flashing squares

--- my project.py
+++ my project.py
@@ -3,14 +3,28 @@ from sense_hat import SenseHat
 from random import randint
 from time import sleep
 sense = SenseHat()
-while True:
-    x = randint(4,7)
-    y = randint(4,7)
+
+
+
+
+def first_quadrant():
+    x = randint(d,e)
+    y = randint(d,e)
     r = (255)
     g = randint(100,255)
     b = randint(0,255)
     sense.set_pixel(x, y, r, g, b)
 
-
+while True:
+    for i in range(2560):
+        d=0
+        e=3
+        first_quadrant()
+    sense.clear()
+    for i in range(2560):
+        d=4
+        e=7
+        first_quadrant()
+    sense.clear()
     
 
